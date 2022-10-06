3a40d1d11a5d90d9e31761d7022deeab2a4f5147
Yahya Qodsi <yqodsi@e2r8p12.1337.ma>
2022-10-06 05:27:55 +0100

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import "./App.css";
-import { BookList } from "./components/BookList";
+import { Book } from "./components/book";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: grid;
+  gap: 2rem;
+  width: 90vw;
+  max-width: 1170px;
+  margin: 5rem auto;
+  place-items: center;
+`;
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_SY400_.jpg",
+  title: "firstBook",
+  author: "hamoda",
+};
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL320_SR320,320_.jpg",
+  title: "secondBook",
+  author: "aziz",
+};
 function App() {
   return (
-    <div>
-      <BookList></BookList>
-    </div>
+    <Wrapper className="booklist">
+      <Book
+        title={secondBook.title}
+        img={secondBook.img}
+        author={secondBook.author}
+      ></Book>
+      <Book
+        title={firstBook.title}
+        img={firstBook.img}
+        author={firstBook.author}
+      ></Book>
+    </Wrapper>
   );
 }
 export default App;
